6380f69a52d851638353dfb3643a7e477bc39e0d
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2018-01-08 14:32:34 +0100

    created code to connect to db

--- src/de/hftstuttgart/snarex/datapoint/main/DatapointManager.java
+++ src/de/hftstuttgart/snarex/datapoint/main/DatapointManager.java
@@ -1,9 +1,20 @@
 package de.hftstuttgart.snarex.datapoint.main;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class DatapointManager {
+	static SessionFactory factory;
 
 	public static void main(String[] args) {
-		
+		try {
+			// open database connection
+			factory = new Configuration().configure().buildSessionFactory();
+			System.out.println("sessionFactory object created successfully. Connection to database is up.");
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
 
 	}
 

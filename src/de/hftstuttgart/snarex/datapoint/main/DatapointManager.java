9c73716142d00490f1e138d9ef40af19501dc4ae
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2018-01-08 12:53:51 +0100

    deleted unused files and moved files into right package

---
+++ src/de/hftstuttgart/snarex/datapoint/main/DatapointManager.java
@@ -0,0 +1,10 @@
+package de.hftstuttgart.snarex.datapoint.main;
+
+public class DatapointManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/de/hftstuttgart/snarex/datapoint/source/Datapoint.java
@@ -0,0 +1,5 @@
+package de.hftstuttgart.snarex.datapoint.source;
+
+public class Datapoint {
+
+}

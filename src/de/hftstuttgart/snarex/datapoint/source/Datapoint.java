b6d17b831da17ecaa8067d0d3db60bf6382989dc
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2018-01-08 12:56:02 +0100

    initialized values in class Datapoint

--- src/de/hftstuttgart/snarex/datapoint/source/Datapoint.java
+++ src/de/hftstuttgart/snarex/datapoint/source/Datapoint.java
@@ -1,5 +1,41 @@
 package de.hftstuttgart.snarex.datapoint.source;
 
 public class Datapoint {
+	private double temperature;
+	private double pressure;
+	private double revolutions;
 
+	public Datapoint() {
+	};
+
+	public Datapoint(double temperature, double pressure, double revolutions) {
+		this.temperature = temperature;
+		this.pressure = pressure;
+		this.revolutions = revolutions;
+	}
+
+	public double getPressure() {
+		return pressure;
+	}
+
+	public void setPressure(double pressure) {
+		this.pressure = pressure;
+	}
+
+	public double getRevolutions() {
+		return revolutions;
+	}
+
+	public void setRevolutions(double revolutions) {
+		this.revolutions = revolutions;
+	}
+
+	public double getTemperature() {
+
+		return temperature;
+	}
+
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
 }

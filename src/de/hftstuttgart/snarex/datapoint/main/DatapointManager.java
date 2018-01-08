2fe97ea41a4c1fb4c19ec8711718564553159638
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2018-01-08 14:37:02 +0100

    created method to read values from db

--- src/de/hftstuttgart/snarex/datapoint/main/DatapointManager.java
+++ src/de/hftstuttgart/snarex/datapoint/main/DatapointManager.java
@@ -1,16 +1,16 @@
 package de.hftstuttgart.snarex.datapoint.main;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import de.hftstuttgart.snarex.datapoint.source.Datapoint;
 
-
-
 public class DatapointManager {
 	static SessionFactory factory;
-	
+
 	// save object in database/table
 	public static void save(Datapoint datapoint) {
 		Session session = factory.openSession();
@@ -19,18 +19,27 @@ public class DatapointManager {
 		session.getTransaction().commit();
 		session.close();
 	}
-	
+
 	// delete object from database/table by id
-		public static void delete(int id) {
-			Session session = factory.openSession();
-			session.beginTransaction();
-			Datapoint dtp = new Datapoint();
-			dtp.setId(id);
-			session.delete(dtp);
-			session.getTransaction().commit();
-			session.close();
-		}
-	
+	public static void delete(int id) {
+		Session session = factory.openSession();
+		session.beginTransaction();
+		Datapoint dtp = new Datapoint();
+		dtp.setId(id);
+		session.delete(dtp);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	// get/read values from database/table
+	public static List<Datapoint> getData() {
+		List<Datapoint> datapoints; // created list named employees
+		Session session = factory.openSession(); // open Session to create SQL argument
+		datapoints = session.createQuery("FROM Datapoint").list();
+		session.close();
+		return datapoints;
+	}
+
 	public static void main(String[] args) {
 		try {
 			// open database connection

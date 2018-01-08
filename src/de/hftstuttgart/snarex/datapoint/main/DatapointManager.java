594ab287a490086f4a454d2f255c3471967bce93
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2018-01-08 19:00:02 +0100

    changed layout

--- src/de/hftstuttgart/snarex/datapoint/main/DatapointManager.java
+++ src/de/hftstuttgart/snarex/datapoint/main/DatapointManager.java
@@ -10,44 +10,15 @@ import org.hibernate.cfg.Configuration;
 import de.hftstuttgart.snarex.datapoint.source.Datapoint;
 
 public class DatapointManager {
-	// initialize session factory
+	/* initialize session factory */
 	static SessionFactory factory;
 
-	// date formatter
+	/* date formatter */
 	static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-	// save object in database/table
-	public static void save(Datapoint datapoint) {
-		Session session = factory.openSession();
-		session.beginTransaction();
-		session.save(datapoint);
-		session.getTransaction().commit();
-		session.close();
-	}
-
-	// delete object from database/table by id
-	public static void delete(int id) {
-		Session session = factory.openSession();
-		session.beginTransaction();
-		Datapoint dtp = new Datapoint();
-		dtp.setId(id);
-		session.delete(dtp);
-		session.getTransaction().commit();
-		session.close();
-	}
-
-	// get/read values from database/table
-	public static List<Datapoint> getData() {
-		List<Datapoint> datapoints; // created list named employees
-		Session session = factory.openSession(); // open Session to create SQL argument
-		datapoints = session.createQuery("FROM Datapoint").list();
-		session.close();
-		return datapoints;
-	}
 
 	public static void main(String[] args) {
 
-		// open database connection
+		/* OPEN database connection */
 		try {
 			factory = new Configuration().configure().buildSessionFactory();
 			System.out.println("sessionFactory object created successfully. Connection to database is up.");
@@ -55,19 +26,28 @@ public class DatapointManager {
 			System.err.println("Failed to create sessionFactory object." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
-
+		/* CREATE, DELETE and READ values */
 		try {
+			/* generate random values */
 			/*
-			 * generate random values 
-			 * for(int i= 0; i<250;i++) { Random r = new Random();
-			 * double rTemperature = r.nextDouble()*100; double rPressure =
-			 * r.nextDouble()*10; double rRevolutions = r.nextDouble()*10000; Datapoint dtp1
-			 * = new Datapoint(LocalDateTime.now(),rTemperature,rPressure,rRevolutions);
-			 * save(dtp1); }
+			 * for(int i= 0; i<250;i++) {
+			 * Random r = new Random(); 
+			 * double rTemperature = r.nextDouble()*100; 
+			 * double rPressure = r.nextDouble()*10;
+			 * double rRevolutions= r.nextDouble()*10000;
+			 * Datapoint dtp1 = new Datapoint(LocalDateTime.now(),rTemperature,rPressure,rRevolutions);
+			 * save(dtp1); 
+			 * }
 			 */
 
+			/*
+			 * Datapoint dtp= new Datapoint(LocalDateTime.now(),30.2,2.1,8000.1); 
+			 * save(dtp);
+			 */
+			
 			// delete(1201);
-
+			
+			/* PRINT values from database/table */
 			List<Datapoint> datapoints = getData();
 			System.out.println("---------------------------------------------------------------------------");
 			System.out.print(
@@ -80,9 +60,38 @@ public class DatapointManager {
 			}
 			System.out.println("---------------------------------------------------------------------------");
 		} finally {
-			// close database connection
+			/* CLOSE database connection */
 			factory.close();
 		}
+	}
+
+	/* SAVE object in database/table */
+	public static void save(Datapoint datapoint) {
+		Session session = factory.openSession();
+		session.beginTransaction();
+		session.save(datapoint);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	/* DELETE object from database/table by id */
+	public static void delete(int id) {
+		Session session = factory.openSession();
+		session.beginTransaction();
+		Datapoint dtp = new Datapoint();
+		dtp.setId(id);
+		session.delete(dtp);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	/* GET/READ values from database/table */
+	public static List<Datapoint> getData() {
+		List<Datapoint> datapoints; // created list named employees
+		Session session = factory.openSession(); // open Session to create SQL argument
+		datapoints = session.createQuery("FROM Datapoint").list();
+		session.close();
+		return datapoints;
 	}
 
 }
